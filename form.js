dc63e794bb5ea0409d991229519ca510f5976192
robbender <robertbender@Roberts-MacBook-Pro-2.local>
2020-06-27 17:57:23 -0700

    first commit

---
+++ form.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("input#submit").click(function() {
+    $.ajax({
+      type: "POST",
+      url: "process.php", //
+      data: $('form.contact').serialize(),
+      success: function(msg) {
+        $("#thanks").html(msg)
+        $("#form-content").modal('hide');
+      },
+      error: function() {
+        alert("failure");
+      }
+    });
+  });
+  // First Modal action with one modal
+  $('.modal').on('hidden.bs.modal', function() {
+    $(this).find('form')[0].reset();
+  });
+
+  // Second Modal Action
+  // $('input#submit').click(function(e){
+  //         e.preventDefault();
+  //
+  //         $('#form-content')
+  //             .modal('hide')
+  //             .on('hidden.bs.modal', function (e) {
+  //                 $('#response').modal('show');
+  //
+  //                 $(this).off('hidden.bs.modal'); // Remove the 'on' event binding
+  //             });
+  //
+  //     });
+});
